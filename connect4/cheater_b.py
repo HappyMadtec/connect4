566ab58abc27ad5400391484a1e3c0a315973c50
HappyMadtec <88161084+HappyMadtec@users.noreply.github.com>
2022-10-16 22:44:54 +0200

    Implementation of cheater_b.py

--- connect4/cheater_b.py
+++ connect4/cheater_b.py
@@ -9,9 +9,9 @@ from connect4.game import Grid, Player, Cell
 class CheaterB(Player):
     """This IA cheats and modify the grid to ensure player B wins."""
 
-    #def play(self, grid: Grid) -> int:
-       # """win horizontal"""
-    """ i = 0
+    def play(self, grid: Grid) -> int:
+        """win horizontal"""
+     i = 0
         j = 0
         for cell in range(grid.lines):
             for cell2 in range(grid.columns):
@@ -26,12 +26,12 @@ class CheaterB(Player):
                 
         print(grid)
         if i == j:
-            return i"""
+            return i
 
-    def play(self, grid: Grid) -> int:
-        """win horizontal"""
+    """def play(self, grid: Grid) -> int:
+        #win horizontal
         for i in range(4):
             grid.place(i, Cell.B)
         return 0
 
-    print(Grid)
+    print(Grid)"""
